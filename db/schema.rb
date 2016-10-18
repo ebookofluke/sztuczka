4bbb514c8c12f15120a426f3645a6f55d0090967
ebookofluke <ebookofluke@protonmail.com>
2016-10-18 20:44:31 +0200

    fix admin error

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161018175456) do
+ActiveRecord::Schema.define(version: 20161018184401) do
 
   create_table "friendly_id_slugs", force: :cascade do |t|
     t.string   "slug",                      null: false
@@ -75,6 +75,7 @@ ActiveRecord::Schema.define(version: 20161018175456) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
